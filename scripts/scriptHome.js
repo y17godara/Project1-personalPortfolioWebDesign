12d692780149e8689c646e5a09849de56acab1e6
YashGodara <yash17godara@gmail.com>
2022-09-11 03:29:22 +0530

    perfect-page

---
+++ scripts/scriptHome.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 20){
+//             $('')
+//         }else{
+
+//         }
+//     })
+// })
+
+
+    
+
